2ffdc6b766617390f119f1c66665159eb0c869ed
Jennie Robinson Faber <jennie@cipritine.local>
2018-05-03 01:18:53 -0400

    Squashed commit of the following:
    commit 9deda56e31c12582f6e71f7483121f066e4d9eec
    Merge: 7df8b1a 0c303da
    Author: Jennie Robinson Faber <jennie@cipritine.local>
    Date:   Thu May 3 01:13:49 2018 -0400
    
        Merge branch 'refresh-2018' of https://github.com/AmandaCaskie/amandaleecaskie into refresh-2018
    
        # Conflicts:
        #	.gitignore
        #	app.js
        #	assets/css/_global.css
        #	assets/css/index.css
        #	package-lock.json
        #	package.json
        #	views/index.sgr
        #	views/layout.sgr
    
    commit 7df8b1a6429efb21dc5b9c9af0f0f567c7efc9e3
    Author: Jennie Robinson Faber <jennie@cipritine.local>
    Date:   Thu May 3 01:12:16 2018 -0400
    
        Refresh
    
    commit 0c303da5e7e9f55d6d4b73b04847d84e5fc27fb9
    Author: Jennie Robinson Faber <jennie@cipritine.local>
    Date:   Wed May 2 01:05:21 2018 -0400
    
        Style refresh

---
+++ app.js
@@ -0,0 +1,57 @@
+const env                     = require('dotenv').config()
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const pageId = require('spike-page-id')
+const sugarml = require('sugarml')
+const sugarss = require('sugarss')
+const df                      = require('dateformat')
+
+const SpikeDatoCMS            = require('spike-datocms')
+const postcssMixins           = require('postcss-mixins')
+
+
+const locals                  = { }
+
+const Dato = new SpikeDatoCMS({
+  addDataTo: locals,
+  token: process.env.dato_api_key,
+  models: [
+    {
+      name: 'home_page',
+      template: {
+        path: 'views/index.sgr',
+        output: (page) => { return `/index.html` }
+      }
+    },
+    {
+      name: 'class',
+      transform: (data) => {
+        if (data.date) {
+          dateHours = new Date(data.date)
+          data.date = dateHours.setUTCHours(5)
+        }
+        return data
+      }
+    }
+  ]
+})
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.css' },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
+  reshape: htmlStandards({
+    parser: sugarml,
+    locals: (ctx) => { return Object.assign(locals,
+      { pageId: pageId(ctx) },
+      { df: df.bind(df) }
+    )}
+  }),
+  postcss: cssStandards({
+    warnForDuplicates: env !== 'production',
+    appendPlugins: postcssMixins()
+
+  }),
+  plugins: [ Dato ]
+}
